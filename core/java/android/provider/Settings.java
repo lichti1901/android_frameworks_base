baaa0b1ce70c4ef546ba2546978fde5349b52d44
Muhammed Nazim <mnm9994u@gmail.com>
2014-12-05 11:33:14 -0800

    Add option to hide AlarmClock Icon in StatusBar [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1972,6 +1972,12 @@ public final class Settings {
         public static final String NEXT_ALARM_FORMATTED = "next_alarm_formatted";
 
         /**
+         * Option to hide the Alarm icon in the status bar.
+         * @hide
+         */
+        public static final String SHOW_ALARM_ICON = "show_alarm_icon";
+
+        /**
          * Scaling factor for fonts, float.
          */
         public static final String FONT_SCALE = "font_scale";
