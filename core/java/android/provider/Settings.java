776edd89cd69a4bd9bfe0c01b583ab5d17f69ebc
Adnan Begovic <adnan@cyngn.com>
2014-12-25 05:25:11 +0000

    Base: forward port Headsup options (1/2)
    -Global on/off
    -Blacklist and Do not disturb
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3111,6 +3111,27 @@ public final class Settings {
 	public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
 
         /**
+         * Heads Up Notifications
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_NOTIFICATION = "heads_up_enabled";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_CUSTOM_VALUES = "heads_up_custom_values";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
