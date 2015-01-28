14f94de97976b49892cce027e3767d7406c3614b
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-01-28 22:30:24 +0100

    Lock screen: Weather panel, (WIP), (1/2):
    Based on CyanogenMod:
    https://github.com/CyanogenMod/android_frameworks_base/commit/d855e463bb934f41cee72a893609626de0a03f25
    https://github.com/CyanogenMod/android_frameworks_base/commit/1511e96f191bc4c9f191e417f2c0ee69058f863a
    
    Additional changes:
    
    - Move the weather panel to the bottom of the clock
    - Option to show/hide the weather panel
    - Option to show/hide the location

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3052,6 +3052,18 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
 
         /**
+         * Whether to show the weather info on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER = "lock_screen_show_weather";
+
+        /**
+         * Whether to show the weather location lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER_LOCATION = "lock_screen_show_weather_location";
+
+        /**
          * Show/hide network speed indicator
          * @hide
          */
