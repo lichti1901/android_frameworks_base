32ff99fe2d66460b21a72b0189e33d799f7faeb5
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:32:00 +0000

    QS: add Brightness tile
    * long press to toggle auto/manual brightness
    
    Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: I173281dbaba5945f0f99071639ddccee6a6ca365

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -36,6 +36,7 @@ public class QSConstants {
     public static final String TILE_NFC = "nfc";
     public static final String TILE_COMPASS = "compass";
     public static final String TILE_HEADS_UP = "toggleHeadsUp";
+    public static final String TILE_BRIGHTNESS = "brightness";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -62,5 +63,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_NFC);
         TILES_AVAILABLE.add(TILE_COMPASS);
 	TILES_AVAILABLE.add(TILE_HEADS_UP);
+        TILES_AVAILABLE.add(TILE_BRIGHTNESS);
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -34,6 +34,7 @@ import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
+import com.android.systemui.qs.tiles.BrightnessTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
@@ -296,6 +297,8 @@ public class QSTileHost implements QSTile.Host {
                 return new NfcTile(this);
 	    case QSConstants.TILE_HEADS_UP:
 		return new HeadsupTile(this);
+            case QSConstants.TILE_BRIGHTNESS:
+                return new BrightnessTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
